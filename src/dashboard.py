2e6179aaf909de418c18149b168a7cacfc3207ce
jedg83 <79536920+jedg83@users.noreply.github.com>
2025-03-12 20:49:52 +0000

    m

--- src/dashboard.py
+++ src/dashboard.py
@@ -7,17 +7,22 @@ st.title("BTC/USD Market Data Dashboard")
 def load_data():
     df = pd.read_csv("data/gold_real_time.csv")
     df["timestamp"] = pd.to_datetime(df["timestamp"])
+    df.set_index("timestamp", inplace=True)
+    st.line_chart(df[["price"]])
     return df
 
 placeholder = st.empty()
 
-while True:
-    data = load_data()
+
+
+
+#while True:
+data = load_data()
     
    
-    placeholder.dataframe(data.tail(10))
+#    placeholder.dataframe(data.tail(10))
     
     
-    st.line_chart(data.set_index("timestamp")["price"])
+#    st.line_chart(data.set_index("timestamp")["price"])
     
-    time.sleep(10)
+#    time.sleep(10)

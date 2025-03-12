b989108b9fa741c039fe66f1452dbee62da9be8b
jedg83 <79536920+jedg83@users.noreply.github.com>
2025-03-12 18:35:07 +0000

    m

--- src/btc_pipeline.py
+++ src/btc_pipeline.py
@@ -2,9 +2,9 @@ import pandas as pd
 
 def ingest_batch_data():
     try:
-        df = pd.read_csv("data/historical_btc.csv")  
+        df = pd.read_csv("data/btc-news-recent-file.csv")  
         df.to_csv("data/bronze_batch.csv", index=False)
-        print("Batch data stored in Bronze layer.")
+        print("Batch data stored in Bronze file.")
     except Exception as e:
         print(f"Error processing data: {e}")
 

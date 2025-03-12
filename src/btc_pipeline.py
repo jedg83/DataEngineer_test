4d82f8593a44f1ae75134dd22012116f5c6fe598
jedg83 <79536920+jedg83@users.noreply.github.com>
2025-03-12 18:42:50 +0000

    m

--- src/btc_pipeline.py
+++ src/btc_pipeline.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 def ingest_batch_data():
     try:
-        df = pd.read_csv("data/btc-news-recent-file.csv")  
+        df = pd.read_csv("https://raw.githubusercontent.com/JaimeSaldarriaga/Data-Engineer/refs/heads/main/btc-news-recent-f.csv")  
         df.to_csv("data/bronze_batch.csv", index=False)
         print("Batch data stored in Bronze file.")
     except Exception as e:

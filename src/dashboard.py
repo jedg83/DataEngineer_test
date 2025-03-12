0c2317ad98cf435663797c449d57ac565beaa3a6
jedg83 <79536920+jedg83@users.noreply.github.com>
2025-03-12 19:09:26 +0000

    m

--- src/dashboard.py
+++ src/dashboard.py
@@ -4,7 +4,6 @@ import time
 
 st.title("BTC/USD Market Data Dashboard")
 
-# Load Data
 def load_data():
     df = pd.read_csv("data/gold_real_time.csv")
     df["timestamp"] = pd.to_datetime(df["timestamp"])
@@ -15,10 +14,10 @@ placeholder = st.empty()
 while True:
     data = load_data()
     
-    # Display latest data
+   
     placeholder.dataframe(data.tail(10))
     
-    # Line chart for BTC prices
+    
     st.line_chart(data.set_index("timestamp")["price"])
     
     time.sleep(10)
--- src/data_tansformation.py
+++ src/data_tansformation.py
@@ -1,20 +1,20 @@
 import pandas as pd
 
-def clean_data(file_path, output_path):
+def cleaning_data(file_path, output_path):
     df = pd.read_csv(file_path)
     
-    # Convert timestamps
+    
     df["timestamp"] = pd.to_datetime(df["timestamp"])
     
-    # Convert price to float
+    
     df["price"] = df["price"].astype(float)
     
-    # Drop duplicates
+    
     df.drop_duplicates(inplace=True)
 
     df.to_csv(output_path, index=False)
     print(f"Cleaned data stored in {output_path}")
 
 if __name__ == "__main__":
-    clean_data("data/bronze_real_time.csv", "data/silver_real_time.csv")
-    clean_data("data/bronze_batch.csv", "data/silver_batch.csv")
+    cleaning_data("data/bronze_real_time.csv", "data/silver_real_time.csv")
+    cleaning_data("data/bronze_batch.csv", "data/silver_batch.csv")
